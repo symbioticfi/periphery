f618a0fc49d6ca736bd34ddda7681cfd294f03c2
Kresh <andrey@symbiotic.fi>
2024-08-01 05:36:44 +0400

    sync with core

--- src/contracts/DefaultCollateralMigrator.sol
+++ src/contracts/DefaultCollateralMigrator.sol
@@ -12,13 +12,18 @@ contract DefaultCollateralMigrator is IDefaultCollateralMigrator {
     /**
      * @inheritdoc IDefaultCollateralMigrator
      */
-    function migrate(address collateral, address vault, address onBehalfOf, uint256 amount) external {
+    function migrate(
+        address collateral,
+        address vault,
+        address onBehalfOf,
+        uint256 amount
+    ) external returns (uint256, uint256) {
         IERC20(collateral).transferFrom(msg.sender, address(this), amount);
         IDefaultCollateral(collateral).withdraw(address(this), amount);
 
         address asset = IDefaultCollateral(collateral).asset();
         amount = IERC20(asset).balanceOf(address(this));
         IERC20(asset).approve(vault, amount);
-        IVault(vault).deposit(onBehalfOf, amount);
+        return IVault(vault).deposit(onBehalfOf, amount);
     }
 }
